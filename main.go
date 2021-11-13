c73a2d32a1ad59cc06cfb77f19d0b1a4d462dc93
Adam <adamnixum@gmail.com>
2021-11-13 21:20:02 +0000

    ep 2 complete

--- main.go
+++ main.go
@@ -1,40 +1,61 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
+
+	"github.com/AdamNixon/testapp/handlers"
 )
 
 func main() {
-	// reqeusts to the path /goodbye with be handled by this function
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World")
-	})
 
-	// any other request will be handled by this function
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Running Hello Handler")
+	bindAddress, found := os.LookupEnv("BIND_ADDRESS")
+	if !found {
+		bindAddress = ":9090"
+	}
 
-		// read the body
-		b, err := ioutil.ReadAll(r.Body)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := http.Server{
+		Addr:         bindAddress,       // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	go func() {
+		l.Println("Starting Server")
+		err := s.ListenAndServe()
 		if err != nil {
-			log.Println("Error reading body", err)
-
-			http.Error(rw, "Unable to read request body", http.StatusBadRequest)
-			return
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
 		}
 
-		// write the response
-		fmt.Fprintf(rw, "Hello %s", b)
-	})
+	}()
 
-	// Listen for connections on all ip addresses (0.0.0.0)
-	// port 9090
-	log.Println("Starting Server")
-	fmt.Println(os.Getenv("TEST_ENV"))
-	err := http.ListenAndServe(":9090", nil)
-	log.Fatal(err)
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 5 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	s.Shutdown(ctx)
 }
